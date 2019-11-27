2da4ab4b1ef4dfb3e8bc9e38e5df29b4e93047c1
RVolders <robbe.volders@gmail.com>
2019-11-27 17:48:20 +0100

    e

--- www/public/index.php
+++ www/public/index.php
@@ -21,7 +21,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require 'www/var/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once 'www/var/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
